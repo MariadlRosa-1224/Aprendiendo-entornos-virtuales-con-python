95934d70e91c7b64ed297cbfd35230e34055541c
bananna2 <118637827+bananna2@users.noreply.github.com>
2023-12-30 14:24:17 -0500

    first commit

---
+++ codigo-curso/1-hello.py
@@ -0,0 +1,5 @@
+""" Esto es solo una prueba para el entorno de desarrollo, nada importante """
+
+print("Hola mundo")
+
+"""Para ejecutar este archivo, debemos abrir la terminal y escribir python3 1-hello.py"""
